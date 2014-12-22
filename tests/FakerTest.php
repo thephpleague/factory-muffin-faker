a0b478316964bc1ed268fb91d6b8c6e734a59a7f
Graham Campbell <graham@mineuk.com>
2014-12-22 20:06:44 +0000

    Complete the tests

--- tests/FakerTest.php
+++ tests/FakerTest.php
@@ -1,6 +1,6 @@
 <?php
 
-/**
+/*
  * This file is part of Factory Muffin Faker.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -16,8 +16,6 @@ use League\FactoryMuffin\Faker\Facade as Faker;
 
 /**
  * This is the faker test class.
- *
- * @group faker
  *
  * @author  Scott Robertson <scottymeuk@gmail.com>
  * @author  Graham Campbell <graham@mineuk.com>
@@ -49,6 +47,40 @@ class FakerTest extends PHPUnit_Framework_TestCase
         $this->assertInternalType('string', $sentence());
         $formatter = Faker::getFormatter('numberBetween');
         $this->assertSame(5, $formatter(5, 5));
+    }
+
+    public function testUnique()
+    {
+        $unique = Faker::unique(false, 5);
+        $result = $unique->numberBetween(42, 42);
+
+        $this->assertInstanceOf('League\FactoryMuffin\Faker\Faker', $unique);
+        $this->assertSame(42, $result());
+
+        try {
+            $result();
+            $this->assertFalse(true, 'An overflow exception was expected to be thrown.');
+        } catch (OverflowException $exception) {
+            $this->assertSame('Maximum retries of 5 reached without finding a unique value', $exception->getMessage());
+        }
+    }
+
+    public function testOptionalFalse()
+    {
+        $optional = Faker::optional(0.0, 'foo');
+        $result = $optional->numberBetween(42, 42);
+
+        $this->assertInstanceOf('League\FactoryMuffin\Faker\Faker', $optional);
+        $this->assertSame('foo', $result());
+    }
+
+    public function testOptionalTrue()
+    {
+        $optional = Faker::optional(1.0, 'foo');
+        $result = $optional->numberBetween(42, 42);
+
+        $this->assertInstanceOf('League\FactoryMuffin\Faker\Faker', $optional);
+        $this->assertSame(42, $result());
     }
 
     public function testReset()
