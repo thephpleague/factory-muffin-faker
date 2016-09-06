2c14df57eef36d4aac780f8c2b9f37c79e6fd8b9
Biser Antonov <biser.antonov@gmail.com>
2016-09-06 21:35:17 +0300

    annotations for code completion

--- src/Facade.php
+++ src/Facade.php
@@ -38,6 +38,144 @@ namespace League\FactoryMuffin\Faker;
  * @mixin \Faker\Provider\Uuid
  *
  * @author Graham Campbell <graham@alt-three.com>
+ *
+ * @method static string name
+ * @method static string firstName
+ * @method static string firstNameMale
+ * @method static string firstNameFemale
+ * @method static string lastName
+ * @method static string title
+ * @method static string titleMale
+ * @method static string titleFemale
+ * @method static string citySuffix
+ * @method static string streetSuffix
+ * @method static string buildingNumber
+ * @method static string city
+ * @method static string streetName
+ * @method static string streetAddress
+ * @method static string postcode
+ * @method static string address
+ * @method static string country
+ * @method static float  latitude
+ * @method static float  longitude
+ * @method static string ean13
+ * @method static string ean8
+ * @method static string isbn13
+ * @method static string isbn10
+ * @method static string phoneNumber
+ * @method static string company
+ * @method static string companySuffix
+ * @method static string jobTitle
+ * @method static string creditCardType
+ * @method static string creditCardNumber($type = null, $formatted = false, $separator = '-')
+ * @method static \DateTime $creditCardExpirationDate
+ * @method static string creditCardExpirationDateString
+ * @method static string creditCardDetails
+ * @method static string bankAccountNumber
+ * @method static string iban($countryCode = null, $prefix = '', $length = null)
+ * @method static string swiftBicNumber
+ * @method static string vat
+ * @method static string word
+ * @method static string|array words($nb = 3, $asText = false)
+ * @method static string sentence($nbWords = 6, $variableNbWords = true)
+ * @method static string|array sentences($nb = 3, $asText = false)
+ * @method static string paragraph($nbSentences = 3, $variableNbSentences = true)
+ * @method static string|array paragraphs($nb = 3, $asText = false)
+ * @method static string text($maxNbChars = 200)
+ * @method static string realText($maxNbChars = 200, $indexSize = 2)
+ * @method static string email
+ * @method static string safeEmail
+ * @method static string freeEmail
+ * @method static string companyEmail
+ * @method static string freeEmailDomain
+ * @method static string safeEmailDomain
+ * @method static string userName
+ * @method static string password($minLength = 6, $maxLength = 20)
+ * @method static string domainName
+ * @method static string domainWord
+ * @method static string tld
+ * @method static string url
+ * @method static string slug($nbWords = 6, $variableNbWords = true)
+ * @method static string ipv4
+ * @method static string ipv6
+ * @method static string localIpv4
+ * @method static string macAddress
+ * @method static int       unixTime
+ * @method static \DateTime dateTime
+ * @method static \DateTime dateTimeAD
+ * @method static string    iso8601
+ * @method static \DateTime dateTimeThisCentury
+ * @method static \DateTime dateTimeThisDecade
+ * @method static \DateTime dateTimeThisYear
+ * @method static \DateTime dateTimeThisMonth
+ * @method static string    amPm
+ * @method static int       dayOfMonth
+ * @method static int       dayOfWeek
+ * @method static int       month
+ * @method static string    monthName
+ * @method static int       year
+ * @method static int       century
+ * @method static string    timezone
+ * @method static string date($format = 'Y-m-d', $max = 'now')
+ * @method static string time($format = 'H:i:s', $max = 'now')
+ * @method static \DateTime dateTimeBetween($startDate = '-30 years', $endDate = 'now')
+ * @method static \DateTime dateTimeInInterval($date = '-30 years', $interval = '+5 days', $timezone = null)
+ * @method static string md5
+ * @method static string sha1
+ * @method static string sha256
+ * @method static string locale
+ * @method static string countryCode
+ * @method static string countryISOAlpha3
+ * @method static string languageCode
+ * @method static string currencyCode
+ * @method static bool boolean($chanceOfGettingTrue = 50)
+ * @method static int    randomDigit
+ * @method static int    randomDigitNotNull
+ * @method static string randomLetter
+ * @method static string randomAscii
+ * @method static int randomNumber($nbDigits = null, $strict = false)
+ * @method static int|string|null randomKey(array $array = array())
+ * @method static int numberBetween($min = 0, $max = 2147483647)
+ * @method static float randomFloat($nbMaxDecimals = null, $min = 0, $max = null)
+ * @method static mixed randomElement(array $array = array('a', 'b', 'c'))
+ * @method static array randomElements(array $array = array('a', 'b', 'c'), $count = 1)
+ * @method static array|string shuffle($arg = '')
+ * @method static array shuffleArray(array $array = array())
+ * @method static string shuffleString($string = '', $encoding = 'UTF-8')
+ * @method static string numerify($string = '###')
+ * @method static string lexify($string = '????')
+ * @method static string bothify($string = '## ??')
+ * @method static string asciify($string = '****')
+ * @method static string regexify($regex = '')
+ * @method static string toLower($string = '')
+ * @method static string toUpper($string = '')
+ * @method static int biasedNumberBetween($min = 0, $max = 100, $function = 'sqrt')
+ * @method static string macProcessor
+ * @method static string linuxProcessor
+ * @method static string userAgent
+ * @method static string chrome
+ * @method static string firefox
+ * @method static string safari
+ * @method static string opera
+ * @method static string internetExplorer
+ * @method static string windowsPlatformToken
+ * @method static string macPlatformToken
+ * @method static string linuxPlatformToken
+ * @method static string uuid
+ * @method static string mimeType
+ * @method static string fileExtension
+ * @method static string file($sourceDirectory = '/tmp', $targetDirectory = '/tmp', $fullPath = true)
+ * @method static string imageUrl($width = 640, $height = 480, $category = null, $randomize = true, $word = null, $gray = false)
+ * @method static string image($dir = null, $width = 640, $height = 480, $category = null, $fullPath = true, $randomize = true, $word = null)
+ * @method static string hexColor
+ * @method static string safeHexColor
+ * @method static string rgbColor
+ * @method static array rgbColorAsArray
+ * @method static string rgbCssColor
+ * @method static string safeColorName
+ * @method static string colorName
+ * @method static Generator optional($weight = 0.5, $default = null)
+ * @method static Generator unique($reset = false, $maxRetries = 10000)
  */
 class Facade
 {
--- src/Faker.php
+++ src/Faker.php
@@ -22,6 +22,142 @@ use Faker\Factory;
  * using the facade.
  *
  * @author Graham Campbell <graham@alt-three.com>
+ *
+ * @method string name
+ * @method string firstName
+ * @method string firstNameMale
+ * @method string firstNameFemale
+ * @method string lastName
+ * @method string title
+ * @method string titleMale
+ * @method string titleFemale
+ * @method string citySuffix
+ * @method string streetSuffix
+ * @method string buildingNumber
+ * @method string city
+ * @method string streetName
+ * @method string streetAddress
+ * @method string postcode
+ * @method string address
+ * @method string country
+ * @method float  latitude
+ * @method float  longitude
+ * @method string ean13
+ * @method string ean8
+ * @method string isbn13
+ * @method string isbn10
+ * @method string phoneNumber
+ * @method string company
+ * @method string companySuffix
+ * @method string jobTitle
+ * @method string creditCardType
+ * @method string creditCardNumber($type = null, $formatted = false, $separator = '-')
+ * @method \DateTime $creditCardExpirationDate
+ * @method string creditCardExpirationDateString
+ * @method string creditCardDetails
+ * @method string bankAccountNumber
+ * @method string iban($countryCode = null, $prefix = '', $length = null)
+ * @method string swiftBicNumber
+ * @method string vat
+ * @method string word
+ * @method string|array words($nb = 3, $asText = false)
+ * @method string sentence($nbWords = 6, $variableNbWords = true)
+ * @method string|array sentences($nb = 3, $asText = false)
+ * @method string paragraph($nbSentences = 3, $variableNbSentences = true)
+ * @method string|array paragraphs($nb = 3, $asText = false)
+ * @method string text($maxNbChars = 200)
+ * @method string realText($maxNbChars = 200, $indexSize = 2)
+ * @method string email
+ * @method string safeEmail
+ * @method string freeEmail
+ * @method string companyEmail
+ * @method string freeEmailDomain
+ * @method string safeEmailDomain
+ * @method string userName
+ * @method string password($minLength = 6, $maxLength = 20)
+ * @method string domainName
+ * @method string domainWord
+ * @method string tld
+ * @method string url
+ * @method string slug($nbWords = 6, $variableNbWords = true)
+ * @method string ipv4
+ * @method string ipv6
+ * @method string localIpv4
+ * @method string macAddress
+ * @method int       unixTime
+ * @method \DateTime dateTime
+ * @method \DateTime dateTimeAD
+ * @method string    iso8601
+ * @method \DateTime dateTimeThisCentury
+ * @method \DateTime dateTimeThisDecade
+ * @method \DateTime dateTimeThisYear
+ * @method \DateTime dateTimeThisMonth
+ * @method string    amPm
+ * @method int       dayOfMonth
+ * @method int       dayOfWeek
+ * @method int       month
+ * @method string    monthName
+ * @method int       year
+ * @method int       century
+ * @method string    timezone
+ * @method string date($format = 'Y-m-d', $max = 'now')
+ * @method string time($format = 'H:i:s', $max = 'now')
+ * @method \DateTime dateTimeBetween($startDate = '-30 years', $endDate = 'now')
+ * @method \DateTime dateTimeInInterval($date = '-30 years', $interval = '+5 days', $timezone = null)
+ * @method string md5
+ * @method string sha1
+ * @method string sha256
+ * @method string locale
+ * @method string countryCode
+ * @method string countryISOAlpha3
+ * @method string languageCode
+ * @method string currencyCode
+ * @method bool boolean($chanceOfGettingTrue = 50)
+ * @method int    randomDigit
+ * @method int    randomDigitNotNull
+ * @method string randomLetter
+ * @method string randomAscii
+ * @method int randomNumber($nbDigits = null, $strict = false)
+ * @method int|string|null randomKey(array $array = array())
+ * @method int numberBetween($min = 0, $max = 2147483647)
+ * @method float randomFloat($nbMaxDecimals = null, $min = 0, $max = null)
+ * @method mixed randomElement(array $array = array('a', 'b', 'c'))
+ * @method array randomElements(array $array = array('a', 'b', 'c'), $count = 1)
+ * @method array|string shuffle($arg = '')
+ * @method array shuffleArray(array $array = array())
+ * @method string shuffleString($string = '', $encoding = 'UTF-8')
+ * @method string numerify($string = '###')
+ * @method string lexify($string = '????')
+ * @method string bothify($string = '## ??')
+ * @method string asciify($string = '****')
+ * @method string regexify($regex = '')
+ * @method string toLower($string = '')
+ * @method string toUpper($string = '')
+ * @method int biasedNumberBetween($min = 0, $max = 100, $function = 'sqrt')
+ * @method string macProcessor
+ * @method string linuxProcessor
+ * @method string userAgent
+ * @method string chrome
+ * @method string firefox
+ * @method string safari
+ * @method string opera
+ * @method string internetExplorer
+ * @method string windowsPlatformToken
+ * @method string macPlatformToken
+ * @method string linuxPlatformToken
+ * @method string uuid
+ * @method string mimeType
+ * @method string fileExtension
+ * @method string file($sourceDirectory = '/tmp', $targetDirectory = '/tmp', $fullPath = true)
+ * @method string imageUrl($width = 640, $height = 480, $category = null, $randomize = true, $word = null, $gray = false)
+ * @method string image($dir = null, $width = 640, $height = 480, $category = null, $fullPath = true, $randomize = true, $word = null)
+ * @method string hexColor
+ * @method string safeHexColor
+ * @method string rgbColor
+ * @method array rgbColorAsArray
+ * @method string rgbCssColor
+ * @method string safeColorName
+ * @method string colorName
  */
 class Faker
 {
